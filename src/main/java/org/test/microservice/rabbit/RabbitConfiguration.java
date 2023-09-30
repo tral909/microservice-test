153076595514f83f6658387c245671628a00854e
tral909 <indorm1992@mail.ru>
2023-10-01 00:19:30 +0300

    Add rabbitmq listener validation

--- src/main/java/org/test/microservice/rabbit/RabbitConfiguration.java
+++ src/main/java/org/test/microservice/rabbit/RabbitConfiguration.java
@@ -5,18 +5,23 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.validation.SmartValidator;
 import org.test.microservice.properties.RabbitProperties;
 
 @Configuration
 @RequiredArgsConstructor
-public class RabbitConfiguration {
+public class RabbitConfiguration implements RabbitListenerConfigurer { // for validation in rabbit consumer
 
     private final RabbitProperties rabbitProperties;
+    private final SmartValidator validator;
 
     @Bean
     public Queue myQueue() {
@@ -43,6 +48,20 @@ public class RabbitConfiguration {
     @Bean
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    // for validation in rabbit consumer
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(validatingHandlerMethodFactory());
+    }
+
+    // for validation in rabbit consumer
+    @Bean
+    public DefaultMessageHandlerMethodFactory validatingHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setValidator(validator);
+        return factory;
     }
 
 }
--- src/main/java/org/test/microservice/rabbit/RabbitConsumer.java
+++ src/main/java/org/test/microservice/rabbit/RabbitConsumer.java
@@ -1,9 +1,13 @@
 package org.test.microservice.rabbit;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import org.test.microservice.database.repository.MessageRepository;
 import org.test.microservice.mapper.MessageMapper;
 import org.test.microservice.rabbit.dto.MessageRabbitDto;
@@ -13,13 +17,14 @@ import java.util.List;
 @Service
 @Log4j2
 @RequiredArgsConstructor
+@Validated
 public class RabbitConsumer {
 
     private final MessageMapper messageMapper;
     private final MessageRepository messageRepository;
 
     @RabbitListener(queues = "${rabbitmq.message.queue-name}")
-    public void receiveMessage(List<MessageRabbitDto> dto) {
+    public void receiveMessage(@Valid @NotEmpty @Payload List<MessageRabbitDto> dto) {
         log.info("RabbitListener dto: {}", dto);
         messageRepository.saveAll(messageMapper.mapRabbitDtoList(dto));
     }
